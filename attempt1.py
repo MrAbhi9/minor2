db20a76c3b52b565fd3c10509dc1f75b1ca981c4
Abhinav Kumar <110718901+MrAbhi9@users.noreply.github.com>
2024-03-23 15:51:20 +0530

    Update main.py

--- attempt1.py
+++ attempt1.py
@@ -1,31 +1,49 @@
+#Setting some standards  updated the count finger function
+#seek / volume up and down continuation added else block so that once the if condtn ends then the else conddtn will run for volume up/down or seek left/right not for pace ele it will stuck in deadlock
+
+
 import cv2
 import mediapipe as mp      # to detect the landmarks
 import pyautogui            # to connect keyboard and the gestures
 import time                 # was unable to detect , it counted 1,2,3 before even 5 finger o multiple outcome were there 
 
-def count_fingers(lst):     # landmark k hisab se finger count kr lenge fir uske hisab se play/pause vgera jo bhi krna h vo kr denge 
+def case_count_fingers(lst):     # landmark k hisab se finger count kr lenge fir uske hisab se play/pause vgera jo bhi krna h vo kr denge 
     cnt = 0
 
     thresh = (lst.landmark[0].y*100  - lst.landmark[9].y*100)/2       #landmark 0-9 ki value ki half value store kr lia thresh me or 100 se multiply sab jgh kra denge taki bada digit dikhe or calculation me asani ho 
 
-    #for any one finger 
-    if(lst.landmark[5].y*100  - lst.landmark[8].y*100 ) > thresh:
-        cnt+=1
+    #for all five finger  :: play /pause
+    if(lst.landmark[5].y*100  - lst.landmark[8].y*100 ) > thresh and (lst.landmark[9].y*100  - lst.landmark[12].y*100 ) > thresh and (lst.landmark[13].y*100  - lst.landmark[16].y*100 ) > thresh and (lst.landmark[17].y*100  - lst.landmark[20].y*100 ) > thresh and (lst.landmark[5].x*100  - lst.landmark[4].x*100 ) > 5:
+        cnt=10
+   
+
+    # upar wale function se right hand k muthi bnd or kholne k liye variable perfect h, left k liye humne thumb k variable ko alag se set kiya
+    elif(lst.landmark[5].y*100  - lst.landmark[8].y*100 ) > thresh and (lst.landmark[9].y*100  - lst.landmark[12].y*100 ) > thresh and (lst.landmark[13].y*100  - lst.landmark[16].y*100 ) > thresh and (lst.landmark[17].y*100  - lst.landmark[20].y*100 ) > thresh and ( lst.landmark[4].x*100 -lst.landmark[5].x*100 ) > 5:
+        cnt=10
+
+    #for the first two finger :: increase volume
+    elif(lst.landmark[5].y*100  - lst.landmark[8].y*100 ) > thresh and (lst.landmark[9].y*100  - lst.landmark[12].y*100 ) > thresh:
+        cnt=2
     
-    #for two finger
-    if(lst.landmark[9].y*100  - lst.landmark[12].y*100 ) > thresh:
-        cnt+=1
+    #for the first finger :: deecrease volume
+    elif(lst.landmark[5].y*100  - lst.landmark[8].y*100 ) > thresh:
+        cnt=1
+    
+    #for seek left 
+    elif(lst.landmark[5].x*100  - lst.landmark[4].x*100 ) > 11:      # pehle 5 value dala tha to kai baar jb apn fist bana rhe the to thoda sa angutha bahar hone k karan seek position me chla ja rha tha ab 11 krne se uski limit badh gyi or system set ho gya  
+        cnt=3
 
-    if(lst.landmark[13].y*100  - lst.landmark[16].y*100 ) > thresh:
-        cnt+=1
+    #for seek right 
+    elif(lst.landmark[4].x*100 - lst.landmark[5].x*100 ) > 11:
+        cnt=4
+    
+    
 
-    if(lst.landmark[17].y*100  - lst.landmark[20].y*100 ) > thresh:
-        cnt+=1
+    else:
+        cnt=0
 
-    #for thumb we have done it in x axis
-    if(lst.landmark[5].x*100  - lst.landmark[4].x*100 ) > 5:
-        cnt+=1
     return cnt
+  
 
 cap = cv2.VideoCapture(0)
 
@@ -47,7 +65,7 @@ while True:
         
         hand_keyPoints = res.multi_hand_landmarks[0]
        
-        cnt = count_fingers(hand_keyPoints)
+        cnt = case_count_fingers(hand_keyPoints)
 
         if not(prev == cnt):
             if not(start_init):
@@ -55,18 +73,37 @@ while True:
                 start_init = True
             elif (end_time-start_time) > 0.2:   #end k time or start time k bich ka antar , taaki multiple na count krle 
                 if (cnt == 1):
-                    pyautogui.press("up")
-                elif (cnt == 2):
                     pyautogui.press("down")
+                elif (cnt == 2):
+                    pyautogui.press("up")
                 elif (cnt == 3):
-                    pyautogui.press("left")
+                    pyautogui.press("left")                    
                 elif (cnt == 4):
-                    pyautogui.press("right")
-                elif (cnt == 5):
-                    pyautogui.press("space")
+                    pyautogui.press("right")                    
+                elif (cnt == 10):
+                    pyautogui.press("space")    
 
                 prev=cnt
                 start_init=False
+        # Adding else block for continuation in the seeking and volume thing 
+        else:
+            if not(start_init):
+                start_time = time.time()        # start hone ka time note kiya, add krte gye 
+                start_init = True
+                #decresed the time a little bit as 0.2 was too large and made the scrolling slow
+            elif (end_time-start_time) > 0.05:   #end k time or start time k bich ka antar , taaki multiple na count krle 
+                if (cnt == 1):
+                    pyautogui.press("down")
+                elif (cnt == 2):
+                    pyautogui.press("up")
+                elif (cnt == 3):
+                    pyautogui.press("left")                    
+                elif (cnt == 4):
+                    pyautogui.press("right")
+                prev=cnt
+                start_init=False
+                
+                
         drawing.draw_landmarks(frm, hand_keyPoints, hands.HAND_CONNECTIONS)     # isse basically red point aaega or vo connected rahega 
     cv2.imshow("window", frm)
 
